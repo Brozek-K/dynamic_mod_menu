e8faf7f49e5a10a6ec8cb1b645721313f9afacfc
bcssov <nxsovereign@gmail.com>
2019-09-10 06:03:35 +0200

    Add localisation categories

--- templates/localisation/dmm_localization_l_english_yml.py
+++ templates/localisation/dmm_localization_l_english_yml.py
@@ -1,15 +1,27 @@
 from templates.utils import settings, templater
 
-template_button = ' dmm_mod_{count}:0 "Dynamic Mod Menu {count}"'
-template_title = ' dmm_mod.{count}.title:0 "Dynamic Mod Menu {count}"'
-template_desc = ' dmm_mod.{count}.desc:0 "Dynamic Mod Menu {count}"'
+template_button = ' dmm_mod{category}_{count}:0 "Dynamic Mod Menu{category_name} {count}"'
+template_title = ' dmm_mod{category}.{count}.title:0 "Dynamic Mod Menu{category_name} {count}"'
+template_desc = ' dmm_mod{category}.{count}.desc:0 "Dynamic Mod Menu{category_name} {count}"'
+
+categories = ["events", "gfx", "utilities", "other"]
 
 
 def process(publish_dir):
     mod_lines = []
     for i in range(1, settings.total + 1):
-        mod_lines.append(template_button.format(count=i))
-        mod_lines.append(template_title.format(count=i))
-        mod_lines.append(template_desc.format(count=i))
+        mod_lines.append(template_button.format(
+            category="", category_name="", count=i))
+        mod_lines.append(template_title.format(
+            category="", category_name="", count=i))
+        mod_lines.append(template_desc.format(
+            category="", category_name="", count=i))
+        for category in categories:
+            mod_lines.append(template_button.format(
+                category="_" + category, category_name=" " + category.capitalize(), count=i))
+            mod_lines.append(template_title.format(
+                category="_" + category, category_name=" " + category.capitalize(), count=i))
+            mod_lines.append(template_desc.format(
+                category="_" + category, category_name=" " + category.capitalize(), count=i))
     templater.process_file(
         publish_dir + "/localisation/english/dmm_localization_l_english.yml", localisation=mod_lines)
