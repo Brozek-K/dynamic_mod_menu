b5cd1d5b0fe9ddc3dc3056f25f0f3fde148a3f7b
bcssov <nxsovereign@gmail.com>
2019-09-10 20:03:07 +0200

    Test publish code comment out

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 py -3 publish.py
-cd publish
-cd dynamic_mod_menu
-Bandizip c -r "dynamic_mod_menu.zip" "*.*"
-xcopy "dynamic_mod_menu.zip" "%UserProfile%\Documents\Paradox Interactive\Stellaris\mod" /y
-cd .. 
-cd ..
-cls
+REM cd publish
+REM cd dynamic_mod_menu
+REM Bandizip c -r "dynamic_mod_menu.zip" "*.*"
+REM xcopy "dynamic_mod_menu.zip" "%UserProfile%\Documents\Paradox Interactive\Stellaris\mod" /y
+REM cd .. 
+REM cd ..
+REM cls
